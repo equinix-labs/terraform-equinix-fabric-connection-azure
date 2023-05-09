343dc43c1c0eddd2442553f81fe1909990de830b
vasubabu <kandimalla.vasubabu@gmail.com>
2023-05-09 19:28:00 +0530

    Add provider equinix and module_name in Equinix Metal TF configs

--- versions.tf
+++ versions.tf
@@ -4,11 +4,14 @@ terraform {
   required_providers {
     equinix = {
       source  = "equinix/equinix"
-      version = ">= 1.7.0"
+      version = "~> 1.14"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 2.72.0"
     }
+  }
+  provider_meta "equinix" {
+    module_name = "equinix-fabric-connection-azure"
   }
 }
